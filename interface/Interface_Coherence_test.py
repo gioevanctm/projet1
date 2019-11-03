f0ed5d7895d5efa0167347f38ef2e579b02c645d
gioctm <gioevan.battery@gmail.com>
2019-11-03 17:45:10 -0400

    test du contenu d'un champ

--- interface/Interface_Coherence_test.py
+++ interface/Interface_Coherence_test.py
@@ -1,36 +1,31 @@
-from tkinter import *
 from tkinter import tix
-import os.path
-from tkinter import ttk
 from tkinter.filedialog import *
-from tkinter.messagebox import *
-from tkinter.ttk import Treeview
-import os
 
+from utilsgio.Coherence import coherence
 
-class interface_coherence(Frame):
+class interface_coherence(Frame):# Création de la fenêtre principale (main window)
     def __init__(self, fenetre, **kwargs):
         Frame.__init__(self, fenetre, width=100, height=100,**kwargs)
-# Création de la fenêtre principale (main window)
         self.pack(fill=BOTH, expand=True)
 
 #initialisation
-        self.liste1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-        self.liste2=[1,2,3,4,1,2,3,4]
-        self.liste3=[1,2,3,4,1,2,3,4,1,2,4]
-        self.liste4=[1,2,3,4,1,2,3,4]
-        self.liste5=[1,2,3,4,1,2,3,4,3]
-        self.value_input1 = StringVar()
-        self.value_input2 = StringVar()
-        self.value_input3 = StringVar()
-        self.value_input4 = StringVar()
-        self.value_input5 = StringVar()
-        self.value_input1.set("Charger un fichier")
-        self.value_input2.set("Charger un fichier")
-        self.value_input3.set("Charger un fichier")
-        self.value_input4.set("Charger un fichier")
-        self.value_input5.set("Charger un fichier")
+        self.listefield1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+        self.listefield2=[1,2,3,4,1,2,3,4]
+        self.listefield3=[1,2,3,4,1,2,3,4,1,2,4]
+        self.listefield4=[1,2,3,4,1,2,3,4]
+        self.listefield5=[1,2,3,4,1,2,3,4,3]
+        self.value_input_zsro = StringVar()
+        self.value_input_pfsro = StringVar()
+        self.value_input_zpbo = StringVar()
+        self.value_input_pfpbo = StringVar()
+        self.value_input_piq = StringVar()
+        self.value_input_zsro.set("Charger la Zone Arrière SRO")
+        self.value_input_pfsro.set("Charger les emplacements SRO")
+        self.value_input_zpbo.set("Charger la Zone arrière PBO")
+        self.value_input_pfpbo.set("Charger les emplacements PBO")
+        self.value_input_piq.set("Charger le Piq_BAL")
         self.varcombo1 = tix.StringVar()
+        self.varcombo11 = tix.StringVar()
         self.varcombo2 = tix.StringVar()
         self.varcombo3 = tix.StringVar()
         self.varcombo4 = tix.StringVar()
@@ -45,6 +40,7 @@ class interface_coherence(Frame):
         self.path3 = StringVar()
         self.path4 = StringVar()
         self.path5 = StringVar()
+        self.coherence = coherence()
 
 # création des Frames dans la fenêtre principale
         self.Frame1 = Frame(self, relief=GROOVE)
@@ -64,14 +60,15 @@ class interface_coherence(Frame):
 # ----------------------------------------------------------------------------------------------------------------------
 #                                               FRAME 1 constructeur
 # ----------------------------------------------------------------------------------------------------------------------
-        self.entree1 = Entry(self.Frame1, textvariable=self.value_input1)  # création de l'entrer
+        self.entree1 = Entry(self.Frame1, textvariable=self.value_input_zsro)  # création de l'entrer
         self.entree1.pack(side=LEFT, fill=X, padx=5, expand=True)  # afficher le champ et le placer (expend true (si plus grand va s'adapter))
         self.combo1 = tix.ComboBox(self.Frame1, editable=1, dropdown=1, variable=self.varcombo1, command=self.Affiche1)
         self.combo1.entry.config(state='readonly')  ## met la zone de texte en lecture seule
-        for i in range(0, len(self.liste1)):
-            self.combo1.insert(i, self.liste1[i])  # Liste est la liste qui récupère les champs
-            i+=1
-        self.combo1.pack(side=LEFT, padx=5, pady=5)
+
+        #AFFICHAGE DU CONTENU DE LA 1ER COLONNE
+        self.combo11 = tix.ComboBox(self.Frame1, editable=1, dropdown=1, variable=self.varcombo11, command=self.Affiche1)
+        self.combo11.entry.config(state='readonly')  ## met la zone de texte en lecture seule
+
         # bouton1 parcourir
         self.bouton1 = Button(self.Frame1, text="Parcourir...", command=self.chose_file1)  # création de bouton
         self.bouton1.pack(side=RIGHT, padx=5, pady=5)  # afficher le bouton et le placer
@@ -79,15 +76,12 @@ class interface_coherence(Frame):
 # ----------------------------------------------------------------------------------------------------------------------
 #                                               FRAME 2 constructeur
 # ----------------------------------------------------------------------------------------------------------------------
-        self.entree2 = Entry(self.Frame2, textvariable=self.value_input2)  # création de l'entrer
+        self.entree2 = Entry(self.Frame2, textvariable=self.value_input_pfsro)  # création de l'entrer
         self.entree2.pack(side=LEFT, fill=X, padx=5, expand=True)  # afficher le champ et le placer (expend true (si plus grand va s'adapter))
         # liste déroulante
         self.combo2 = tix.ComboBox(self.Frame2, editable=1, dropdown=1, variable=self.varcombo2, command=self.Affiche2)
         self.combo2.entry.config(state='readonly')  ## met la zone de texte en lecture seule
-        for i in range(0, len(self.liste2)):
-            self.combo2.insert(i, self.liste1[i])  # Liste est la liste qui récupère les champs
-            i+=1
-        self.combo2.pack(side=LEFT, padx=5, pady=5)
+
         # bouton parcourir
         self.bouton2 = Button(self.Frame2, text="Parcourir...", command=self.chose_file2)  # création de bouton
         self.bouton2.pack(side=RIGHT, padx=5, pady=5)  # afficher le bouton et le placer
@@ -95,17 +89,14 @@ class interface_coherence(Frame):
 #-----------------------------------------------------------------------------------------------------------------------
 #                                               FRAME 3 constructeur
 #-----------------------------------------------------------------------------------------------------------------------
-        self.entree3 = Entry(self.Frame3, textvariable=self.value_input3)  # création de l'entrer
+        self.entree3 = Entry(self.Frame3, textvariable=self.value_input_zpbo)  # création de l'entrer
         self.entree3.pack(side=LEFT, fill=X, padx=5, expand=True)  # afficher le champ et le placer (expend true (si plus grand va s'adapter))
 
     # liste déroulante
 
         self.combo3 = tix.ComboBox(self.Frame3, editable=1, dropdown=1, variable=self.varcombo3, command=self.Affiche3)
         self.combo3.entry.config(state='readonly')  ## met la zone de texte en lecture seule
-        for i in range(0, len(self.liste3)):
-            self.combo3.insert(i, self.liste1[i])  # Liste est la liste qui récupère les champs
-            i+=1
-        self.combo3.pack(side=LEFT, padx=5, pady=5)
+
     # bouton parcourir
         self.bouton3 = Button(self.Frame3, text="Parcourir...", command=self.chose_file3)  # création de bouton
         self.bouton3.pack(side=RIGHT, padx=5, pady=5)  # afficher le bouton et le placer
@@ -113,32 +104,26 @@ class interface_coherence(Frame):
 # -----------------------------------------------------------------------------------------------------------------------
 #                                               FRAME 4 constructeur
 # -----------------------------------------------------------------------------------------------------------------------
-        self.entree4 = Entry(self.Frame4, textvariable=self.value_input4)  # création de l'entrer
+        self.entree4 = Entry(self.Frame4, textvariable=self.value_input_pfpbo)  # création de l'entrer
         self.entree4.pack(side=LEFT, fill=X, padx=5, expand=True)  # afficher le champ et le placer (expend true (si plus grand va s'adapter))
         # liste déroulante
-        combo4 = tix.ComboBox(self.Frame4, editable=1, dropdown=1, variable=self.varcombo4, command=self.Affiche4)
-        combo4.entry.config(state='readonly')  ## met la zone de texte en lecture seule
-        for i in range(0, len(self.liste4)):
-            combo4.insert(i, self.liste1[i])  # Liste est la liste qui récupère les champs
-            i+=1
-        combo4.pack(side=LEFT, padx=5, pady=5)
+        self.combo4 = tix.ComboBox(self.Frame4, editable=1, dropdown=1, variable=self.varcombo4, command=self.Affiche4)
+        self.combo4.entry.config(state='readonly')  ## met la zone de texte en lecture seule
+
         # bouton parcourir
         self.bouton4 = Button(self.Frame4, text="Parcourir...", command=self.chose_file4)  # création de bouton
         self.bouton4.pack(side=RIGHT, padx=5, pady=5)  # afficher le bouton et le placer
 # ----------------------------------------------------------------------------------------------------------------------
 #                                               FRAME 5 constructeur
 # ----------------------------------------------------------------------------------------------------------------------
-        self.entree5 = Entry(self.Frame5, textvariable=self.value_input5)  # création de l'entrer
+        self.entree5 = Entry(self.Frame5, textvariable=self.value_input_piq)  # création de l'entrer
         self.entree5.pack(side=LEFT, fill=X, padx=5, expand=True)  # afficher le champ et le placer (expend true (si plus grand va s'adapter))
 
         # liste déroulante
 
         self.combo5 = tix.ComboBox(self.Frame5, editable=1, dropdown=1, variable=self.varcombo5, command=self.Affiche5)
         self.combo5.entry.config(state='readonly')  ## met la zone de texte en lecture seule
-        for i in range(0, len(self.liste5)):
-            self.combo5.insert(i, self.liste1[i])  # Liste est la liste qui récupère les champs
-            i+=1
-        self.combo5.pack(side=LEFT, padx=5, pady=5)
+
 
         # bouton parcourir
         self.bouton5 = Button(self.Frame5, text="Parcourir...", command=self.chose_file5)  # création de bouton
@@ -162,8 +147,21 @@ class interface_coherence(Frame):
 
 #Définition des chemins
     def chose_file1(self):
-        self.path1 = askopenfilename(title="Ouverture fichier excel", filetypes=[('Excel files', '*.xlsx; *.xls')])  # ouvrir un fichier
-        self.value_input1.set(self.path1)  # value_imput devient le chemin
+        self.path1 = askopenfilename(title="Chagement des Zones arrières SRO", filetypes=[('ShapeFiles', '*.shp'), ('All files', '*.*')])  # ouvrir un fichier des Zones arrières SRO
+        self.value_input_zsro.set(self.path1)  # value_imput devient le chemin
+
+        self.listefield1 = self.coherence.loadShpfile1(self.path1)
+        for i in range(0, len(self.listefield1)):
+            self.combo1.insert(i, self.listefield1[i])  # Liste est la liste qui récupère les champs
+            i+=1
+        self.combo1.pack(side=LEFT, padx=5, pady=5)
+
+        # AFFICHAGE DU CONTENU DE LA 1ER COLONNE
+        self.loadShp1 = self.coherence.loadShp1(self.path1)
+        for i in range(0, len(self.loadShp1)):
+            self.combo11.insert(i, self.loadShp1[i])  # Liste est la liste qui récupère les champs
+            i+=1
+        self.combo11.pack(side=LEFT, padx=5, pady=5)
 
 #-----------------------------------------------------------------------------------------------------------------------
 #                                               FRAME 2 fonction
@@ -175,8 +173,14 @@ class interface_coherence(Frame):
 
 #Définition des chemins
     def chose_file2(self):
-        self.path2 = askopenfilename(title="Ouverture fichier excel", filetypes=[('Excel files', '*.xlsx; *.xls')])  # ouvrir un fichier
-        self.value_input2.set(self.path2)  # value_imput devient le chemin
+        self.path2 = askopenfilename(title="Chagement des emplacements SRO", filetypes=[('ShapeFiles', '*.shp'), ('All files', '*.*')])  # ouvrir un fichier emplacement des SRO
+        self.value_input_pfsro.set(self.path2)  # value_imput devient le
+
+        self.listefield2 = self.coherence.loadShpfile2(self.path2)
+        for i in range(0, len(self.listefield2)):
+            self.combo2.insert(i, self.listefield2[i])  # Liste est la liste qui récupère les champs
+            i +=1
+        self.combo2.pack(side=LEFT, padx=5, pady=5)
 
 #-----------------------------------------------------------------------------------------------------------------------
 #                                               FRAME 3 fonction
@@ -188,8 +192,15 @@ class interface_coherence(Frame):
 
 #Définition des chemins
     def chose_file3(self):
-        self.path3 = askopenfilename(title="Ouverture fichier excel", filetypes=[('Excel files', '*.xlsx; *.xls')])  # ouvrir un fichier
-        self.value_input3.set(self.path3)  # value_imput devient le chemin
+        self.path3 = askopenfilename(title="Chagement des Zones arrières PBO", filetypes=[('ShapeFiles', '*.shp'), ('All files', '*.*')])  # ouvrir un fichier des Zones arrières PBO
+        self.value_input_zpbo.set(self.path3)  # value_imput devient le chemin
+
+        self.listefield3 = self.coherence.loadShpfile3(self.path3)
+        for i in range(0, len(self.listefield3)):
+            self.combo3.insert(i, self.listefield3[i])  # Liste est la liste qui récupère les champs
+            i+=1
+        self.combo3.pack(side=LEFT, padx=5, pady=5)
+
 #-----------------------------------------------------------------------------------------------------------------------
 #                                               FRAME 4 fonction
 #-----------------------------------------------------------------------------------------------------------------------
@@ -200,8 +211,14 @@ class interface_coherence(Frame):
 
 #Définition des chemins
     def chose_file4(self):
-        self.path4 = askopenfilename(title="Ouverture fichier excel", filetypes=[('Excel files', '*.xlsx; *.xls')])  # ouvrir un fichier
-        self.value_input4.set(self.path4)  # value_imput devient le chemin
+        self.path4 = askopenfilename(title="Chagement des emplacements PBO", filetypes=[('ShapeFiles', '*.shp'), ('All files', '*.*')])  # ouvrir un fichier des emplacement des PBO
+        self.value_input_pfpbo.set(self.path4)  # value_imput devient le chemin
+
+        self.listefield4 = self.coherence.loadShpfile4(self.path4)
+        for i in range(0, len(self.listefield4)):
+            self.combo4.insert(i, self.listefield4[i])  # Liste est la liste qui récupère les champs
+            i+=1
+        self.combo4.pack(side=LEFT, padx=5, pady=5)
 
 #-----------------------------------------------------------------------------------------------------------------------
 #                                               FRAME 5 fonction
@@ -213,8 +230,14 @@ class interface_coherence(Frame):
 
 #Définition des chemins
     def chose_file5(self):
-        self.path5 = askopenfilename(title="Ouverture fichier excel", filetypes=[('Excel files', '*.xlsx; *.xls')])  # ouvrir un fichier
-        self.value_input5.set(self.path5)  # value_imput devient le chemin
+        self.path5 = askopenfilename(title="Chagement du Piq_BAL", filetypes=[('ShapeFiles', '*.shp'), ('All files', '*.*')])  # ouvrir un fichier des emplacements des logements
+        self.value_input_piq.set(self.path5)  # value_imput devient le chemin
+
+        self.listefield5 = self.coherence.loadShpfile5(self.path5)
+        for i in range(0, len(self.listefield5)):
+            self.combo5.insert(i, self.listefield5[i])  # Liste est la liste qui récupère les champs
+            i+=1
+        self.combo5.pack(side=LEFT, padx=5, pady=5)
 
 
 fenetre = tix.Tk()
---
+++ utilsgio/Coherence_test.py
@@ -0,0 +1,64 @@
+from tkinter import *
+from tkinter import tix
+import os.path
+from tkinter.filedialog import *
+from tkinter.messagebox import *
+import os
+import shapefile as sf
+from shapely.geometry import Polygon
+
+class coherence:
+    # Création de la Classe
+    def __init__(self): #initialisation des attributs
+        self.shp_zsro = None
+        self.shp_pfsro = None
+        self.shp_zpbo = None
+        self.shp_pfpbo = None
+        self.shp_piq = None
+        self.shp_error = None
+
+
+
+    def loadShpfile1(self, path1):
+        listefield =[]
+        self.shp_zsro = sf.Reader(path1)
+        for field in self.shp_zsro.fields:
+            listefield.append(field[0])
+        return listefield[1:]
+
+    def loadShpfile2(self, path2):
+        listefield =[]
+        self.shp_pfsro = sf.Reader(path2)
+        for field in self.shp_pfsro.fields:
+            listefield.append(field[0])
+        return listefield[1:]
+
+    def loadShpfile3(self, path3):
+        listefield =[]
+        self.shp_zpbo = sf.Reader(path3)
+        for field in self.shp_zpbo.fields:
+            listefield.append(field[0])
+        return listefield[1:]
+
+    def loadShpfile4(self, path4):
+        listefield =[]
+        self.shp_pfpbo = sf.Reader(path4)
+        for field in self.shp_pfpbo.fields:
+            listefield.append(field[0])
+        return listefield[1:]
+
+    def loadShpfile5(self, path5):
+        listefield =[]
+        self.shp_piq = sf.Reader(path5)
+        for field in self.shp_piq.fields:
+            listefield.append(field[0])
+        return listefield[1:]
+
+    # AFFICHAGE DU CONTENU DE LA 1ER COLONNE
+    def loadShp1(self, path1):
+        liste_rec = []
+        self.shp_zsro_rec = sf.Reader(path1)
+        for record in self.shp_zsro_rec.records():
+            liste_rec.append(record[0])
+        return liste_rec[:]
+        print(liste_rec[:])
